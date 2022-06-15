71ff6e637fefb9e21748150bef7c9dbd218f422a
aphex <wenlogic@protonmail.com>
2022-06-15 11:09:23 +0100

    format everything and fix lintstaged

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 
 export const App: FC = () => {
-    return (
-        <div>
-            <h1 style={{ textAlign: 'center' }}>testing stuff ✌️</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>testing stuff ✌️</h1>
+    </div>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    root: 'src',
-    publicDir: 'src',
-    build: {
-        outDir: '../dist',
-    },
+  plugins: [react()],
+  root: 'src',
+  publicDir: 'src',
+  build: {
+    outDir: '../dist',
+  },
 });
