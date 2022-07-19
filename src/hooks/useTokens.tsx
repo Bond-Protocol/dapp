b7a87a063abd7b62f5799c6e338d48e377ebbc74
spaceturtleship <spaceturtleship@protonmail.com>
2022-07-19 19:02:54 +0100

    Add comments and small refactors

--- src/hooks/useTokens.tsx
+++ src/hooks/useTokens.tsx
@@ -6,15 +6,28 @@ import {CustomPriceSource, SupportedPriceSource} from "@bond-labs/bond-library";
 import axios, {AxiosResponse} from "axios";
 import {useQuery} from "react-query";
 
+export interface Price {
+  price: string;
+  source: string;
+}
+
 export function useTokens() {
   const endpoints = getSubgraphEndpoints();
 
   const [mainnet, setMainnet] = useState<Token[]>([]);
   const [testnet, setTestnet] = useState<Token[]>([]);
-  const [currentPrices, setCurrentPrices] = useState(new Map());
+  const [currentPrices, setCurrentPrices] = useState(new Map<string, Price[]>);
 
+  /*
+  Tokens can be present on multiple chains, and thus have differing ids in our library and subgraph.
+  However, they share price API ids, so this convenience function provides a Set with no duplicates.
+   */
   const apiIds = bondLibrary.getUniqueApiIds();
 
+  /*
+  Load the data from the subgraph.
+  Unfortunately we currently need a separate endpoint for each chain, and a separate set of GraphQL queries for each chain.
+   */
   const {data: rinkebyData} = useListTokensRinkebyQuery({
     endpoint: endpoints[0],
   });
@@ -23,6 +36,9 @@ export function useTokens() {
     endpoint: endpoints[1],
   });
 
+  /*
+  Loads token price data from Coingecko.
+   */
   const coingeckoQuery = useQuery("coingeckoData", async () => {
     const tokenIds = [...apiIds.coingecko].join(",");
     try {
@@ -34,6 +50,9 @@ export function useTokens() {
     }
   });
 
+  /*
+  Loads token price data from Nomics.
+   */
   const nomicsQuery = useQuery("nomicsData", async () => {
     const apiKey: string = import.meta.env.VITE_NOMICS_API_KEY;
     const tokenIds = [...apiIds.nomics].join(",");
@@ -50,49 +69,75 @@ export function useTokens() {
     }
   });
 
+
+  /*
+  Loads custom price data. As these may be from varying sources, it is the responsibility of the customPriceFunction
+  defined on the Token in the bond-library to ensure these requests return a price string on success.
+   */
   const customPriceQuery = useQuery("customPriceData", async () => {
     const requests: Set<Promise<string>> = new Set();
     const functions: Map<() => Promise<string>, string[]> = new Map();
     const pricesMap = new Map();
     try {
-      bondLibrary.TOKENS.forEach((value: bondLibrary.Token, key: string) => {
-        value.priceSources.forEach((priceSource: any | CustomPriceSource) => {
-          if (priceSource.customPriceFunction != undefined) {
-            const currentValue = functions.get(priceSource.customPriceFunction) || [];
+      bondLibrary.TOKENS.forEach((token: bondLibrary.Token, key: string) => {
+        token.priceSources.forEach((priceSource: any | CustomPriceSource) => {
+          if (priceSource.source === "custom") {
+            /*
+            Tokens can be present on multiple chains, and thus have differing ids in our library and subgraph.
+            To avoid making a custom price request for each instance of the token, we use a map with the function
+            as the key, and a list of token IDs which use this function as the value.
 
-            if (currentValue.length === 0) {
-              priceSource.customPriceFunction &&
+            First, we get an array of Token IDs which use this function, or an empty array if there are none.
+           */
+            const tokenIds = functions.get(priceSource.customPriceFunction) || [];
+
+            // If this function hasn't been added already, add it to the requests Set
+            if (tokenIds.length === 0) {
               requests.add(
-                priceSource.customPriceFunction().then((result: string) => {
-                  currentValue.forEach((priceSource: string) => {
-                    pricesMap.set(priceSource, result);
-                  });
-                  return result;
-                })
+                priceSource.customPriceFunction()
+                  .then((result: string) => {
+                    // When the request resolves, store the price in the pricesMap,
+                    // using the priceSource as the keyand the result (price) as the value.
+                    tokenIds.forEach((priceSource: string) => {
+                      pricesMap.set(priceSource, result);
+                    });
+                    return result;
+                  })
               );
             }
 
-            currentValue.push(key);
-            priceSource.customPriceFunction &&
-            functions.set(priceSource.customPriceFunction, currentValue);
+            // Add the current Token ID to the array and set it as the value for this function's key.
+            tokenIds.push(key);
+            functions.set(priceSource.customPriceFunction, tokenIds);
           }
-
         });
       });
     } catch (e: any) {
       throw new Error("Error loading custom prices", e);
     }
 
-    return Promise.all(requests).then(() => pricesMap);
+    // When all the Promises have settled, return the pricesMap, which was populated by each request's 'then' callback.
+    return Promise.allSettled(requests).then(() => pricesMap);
   });
 
+  /*
+  When all the price data has been loaded, we can populate the currentPricesMap which this hook will make available.
+  The map uses Token ID as the key, and an array of Price objects as the value.
+
+  The Price array is ordered by priority, based on the PriceSource's key as set in the bond-library.
+  For example, if the Coingecko price has key 0 and the Nomics price has key 1 in the bond-library's Token.priceSources
+  map, then the Coingecko price will be put in prices[0] and the Nomics price in prices[1].
+
+  The Coingecko price will therefore be the default, Nomics the fallback.
+  */
   useEffect(() => {
     if (coingeckoQuery.data && nomicsQuery.data && customPriceQuery.data) {
-      const currentPricesMap = new Map();
+      const currentPricesMap = new Map<string, Price[]>();
 
       bondLibrary.TOKENS.forEach((value: bondLibrary.Token, tokenKey: string) => {
-        const prices: { price: string, source: string }[] = [];
+        const prices: Price[] = [];
         value.priceSources.forEach((priceSource: SupportedPriceSource | CustomPriceSource, priority: number) => {
+          // First, we get the price from the appropriate query data, according to the PriceSource's source field.
           let price;
           switch (priceSource.source) {
           case "coingecko":
@@ -105,6 +150,8 @@ export function useTokens() {
             price = customPriceQuery.data.get(tokenKey);
             break;
           }
+          // Then, we insert it into the prices array, at position [priority],
+          // where priority is the PriceSource's key in the Token.priceSources map.
           prices[priority] = {
             price: price,
             source: priceSource.source
@@ -117,14 +164,22 @@ export function useTokens() {
     }
   }, [coingeckoQuery.data, nomicsQuery.data, customPriceQuery.data]);
 
+  /*
+  We get a list of all tokens being used in the app by concatenating the .tokens data from each Subgraph request.
+   */
   useEffect(() => {
     if (rinkebyData && rinkebyData.tokens && goerliData && goerliData.tokens) {
-      const allMarkets = rinkebyData.tokens.concat(goerliData.tokens);
+      const allTokens = rinkebyData.tokens.concat(goerliData.tokens);
       // @ts-ignore
-      setTestnet(allMarkets);
+      setTestnet(allTokens);
     }
   }, [rinkebyData, goerliData]);
 
+  /*
+  mainnet:        An array of all Tokens the Subgraph has picked up on mainnet networks
+  testnet:        An array of all Tokens the Subgraph has picked up on testnet networks
+  currentPrices:  A map with Token ID as key and an array of Price objects ordered by priority as value
+   */
   return {
     mainnet: mainnet,
     testnet: testnet,
