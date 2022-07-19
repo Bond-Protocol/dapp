694d6de4e3bed86ae2a53f2407f9ad121ea38411
spaceturtleship <spaceturtleship@protonmail.com>
2022-07-19 14:06:32 +0100

    Use react query for price loading calls

--- src/hooks/useTokens.tsx
+++ src/hooks/useTokens.tsx
@@ -4,6 +4,7 @@ import {useEffect, useState} from "react";
 import * as bondLibrary from "@bond-labs/bond-library";
 import {CustomPriceSource, SupportedPriceSource} from "@bond-labs/bond-library";
 import axios, {AxiosResponse} from "axios";
+import {useQuery} from "react-query";
 
 export function useTokens() {
   const endpoints = getSubgraphEndpoints();
@@ -11,9 +12,6 @@ export function useTokens() {
   const [mainnet, setMainnet] = useState<Token[]>([]);
   const [testnet, setTestnet] = useState<Token[]>([]);
   const [currentPrices, setCurrentPrices] = useState(new Map());
-  const [customPriceData, setCustomPriceData] = useState(new Map());
-  const [coingeckoData, setCoingeckoData] = useState(null);
-  const [nomicsData, setNomicsData] = useState(null);
 
   const apiIds = bondLibrary.getUniqueApiIds();
 
@@ -25,41 +23,34 @@ export function useTokens() {
     endpoint: endpoints[1],
   });
 
-  async function getCoingeckoData() {
-    console.log("cdc");
+  const coingeckoQuery = useQuery("coingeckoData", async () => {
     const tokenIds = [...apiIds.coingecko].join(",");
-    let resp: AxiosResponse;
-
     try {
-      resp = await axios.get(
+      return axios.get(
         `https://api.coingecko.com/api/v3/simple/price?ids=${tokenIds}&vs_currencies=usd`
-      );
-
-      setCoingeckoData(resp.data);
+      ).then((response: AxiosResponse) => response.data);
     } catch (e) {
       console.log("coingecko api error: ", e);
     }
-  }
+  });
 
-  async function getNomicsData() {
+  const nomicsQuery = useQuery("nomicsData", async () => {
     const apiKey: string = import.meta.env.VITE_NOMICS_API_KEY;
     const tokenIds = [...apiIds.nomics].join(",");
-    let resp: AxiosResponse;
     try {
-      resp = await axios.get(
+      return axios.get(
         `https://api.nomics.com/v1/currencies/ticker?ids=${tokenIds}&key=${apiKey}`
-      );
-      const nomicsMap = resp.data.reduce(function (result: Map<string, string>, item: { id: string, price: string }) {
-        return result.set(item.id, item.price);
-      }, new Map());
-
-      setNomicsData(nomicsMap);
+      ).then((response: AxiosResponse) => {
+        return response.data.reduce(function (result: Map<string, string>, item: { id: string, price: string }) {
+          return result.set(item.id, item.price);
+        }, new Map());
+      });
     } catch (e) {
       console.log("Nomics API error: ", e);
     }
-  }
+  });
 
-  async function getCustomPrices() {
+  const customPriceQuery = useQuery("customPriceData", async () => {
     const requests: Set<Promise<string>> = new Set();
     const functions: Map<() => Promise<string>, string[]> = new Map();
     const pricesMap = new Map();
@@ -89,12 +80,11 @@ export function useTokens() {
       console.log("Error loading custom prices: ", e);
     }
 
-    await Promise.all(requests);
-    setCustomPriceData(pricesMap);
-  }
+    return Promise.all(requests).then(() => pricesMap);
+  });
 
   useEffect(() => {
-    if (coingeckoData && nomicsData && customPriceData) {
+    if (coingeckoQuery.data && nomicsQuery.data && customPriceQuery.data) {
       const currentPricesMap = new Map();
       const coingeckoPricesMap = new Map();
       const nomicsPricesMap = new Map();
@@ -105,15 +95,15 @@ export function useTokens() {
           let price;
           switch (priceSource.source) {
           case "coingecko":
-            price = coingeckoData[priceSource.apiId] && coingeckoData[priceSource.apiId].usd;
+            price = coingeckoQuery.data[priceSource.apiId] && coingeckoQuery.data[priceSource.apiId].usd;
             coingeckoPricesMap.set(tokenKey, price);
             break;
           case "nomics":
-            price = nomicsData.get(priceSource.apiId);
+            price = nomicsQuery.data.get(priceSource.apiId);
             nomicsPricesMap.set(tokenKey, price);
             break;
           case "custom":
-            price = customPriceData.get(tokenKey);
+            price = customPriceQuery.data.get(tokenKey);
             break;
           }
           prices[priority] = {
@@ -126,23 +116,19 @@ export function useTokens() {
 
       setCurrentPrices(currentPricesMap);
     }
-  }, [coingeckoData, nomicsData, customPriceData]);
+  }, [coingeckoQuery.data, nomicsQuery.data, customPriceQuery.data]);
 
   useEffect(() => {
     if (rinkebyData && rinkebyData.tokens && goerliData && goerliData.tokens) {
       const allMarkets = rinkebyData.tokens.concat(goerliData.tokens);
       // @ts-ignore
       setTestnet(allMarkets);
-      void getCoingeckoData();
-      void getNomicsData();
-      void getCustomPrices();
     }
   }, [rinkebyData, goerliData]);
 
   return {
     mainnet: mainnet,
     testnet: testnet,
-    customPrices: customPriceData,
     currentPrices: currentPrices,
   };
 }
