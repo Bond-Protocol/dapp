7989463571e7acb6111deedbb80fc843cee9365d
spaceturtleship <spaceturtleship@protonmail.com>
2022-08-09 14:38:58 +0100

    Rework market grouping, split into verified/unverified

--- src/components/organisms/IssuerList.tsx
+++ src/components/organisms/IssuerList.tsx
@@ -1,20 +1,36 @@
 import {FC, useEffect, useState} from "react";
 import {useCalculatedMarkets} from "hooks";
+import {getProtocolByAddress} from "@bond-labs/bond-library";
+import {ethers} from "ethers";
 
 export const IssuerList: FC<any> = () => {
   const {allMarkets} = useCalculatedMarkets();
 
-  const [groupedMarkets, setGroupedMarkets] = useState(new Map());
+  const [verifiedMarkets, setVerifiedMarkets] = useState(new Map());
+  const [unverifiedMarkets, setUnverifiedMarkets] = useState(new Map());
 
   useEffect(() => {
     const markets = Array.from(allMarkets.values());
-    const grouped = markets.reduce((result, currentValue) => {
-      const value = result.get(currentValue.owner) || [];
-      value.push(currentValue);
-      result.set(currentValue.owner, value);
-      return result;
-    }, new Map());
-    setGroupedMarkets(grouped);
+
+    const verified = new Map();
+    const unverified = new Map();
+
+    markets.forEach(market => {
+      const protocol = getProtocolByAddress(market.owner, market.network);
+      const id = protocol?.id || market.owner;
+      if (ethers.utils.isAddress(id)) {
+        const value = unverified.get(market.owner) || [];
+        value.push(market);
+        unverified.set(id, value);
+      } else {
+        const value = verified.get(protocol?.id) || [];
+        value.push(market);
+        verified.set(id, value);
+      }
+    });
+
+    setVerifiedMarkets(verified);
+    setUnverifiedMarkets(unverified);
   }, [allMarkets]);
 
   return (
