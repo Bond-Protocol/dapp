e487b621f9a56161d8aff2ea5f7a57fc050aaf42
spaceturtleship <spaceturtleship@protonmail.com>
2022-09-08 06:18:43 -0700

    Fix datepicker month arrows

--- src/components/molecules/DatePicker.tsx
+++ src/components/molecules/DatePicker.tsx
@@ -1,8 +1,8 @@
 import "react-day-picker/dist/style.css";
-import { DayPicker } from "react-day-picker";
-import { useEffect, useState } from "react";
-import { getUserLocale } from "../../utils/getLocale";
-import { ClickAwayListener } from "@mui/base";
+import {DayPicker} from "react-day-picker";
+import {useEffect, useState} from "react";
+import {getUserLocale} from "../../utils/getLocale";
+import {ClickAwayListener} from "@mui/base";
 
 export type DatePickerProps = {
   onChange?: (date?: Date) => void;
@@ -31,23 +31,28 @@ export const DatePicker = ({ onChange, ...props }: DatePickerProps) => {
   }, [onChange, date]);
 
   return (
-    <ClickAwayListener onClickAway={() => handleClose(date)}>
-      <div className={props.className} onClick={() => setOpen((prev) => !prev)}>
-        {props.label && <p>{props.label}</p>}
-        <div
-          className={`px-4 h-10 py-2 border rounded-lg hover:cursor-pointer ${props.dateClassName}`}
-        >
-          <p>{date ? formattedDate : props.placeholder}</p>
-        </div>
-        {open && (
-          <DayPicker
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            className="absolute p-3 backdrop-blur-xl border rounded-lg z-10"
-          />
-        )}
-      </div>
-    </ClickAwayListener>
+      <ClickAwayListener onClickAway={() => handleClose(date)}>
+        <>
+          <div
+              className={props.className}
+              onClick={() => setOpen((prev) => !prev)}
+          >
+            {props.label && <p>{props.label}</p>}
+            <div
+                className={`px-4 h-10 py-2 border rounded-lg hover:cursor-pointer ${props.dateClassName}`}
+            >
+              <p>{date ? formattedDate : props.placeholder}</p>
+            </div>
+          </div>
+          {open && (
+              <DayPicker
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  className="absolute p-3 backdrop-blur-xl border rounded-lg z-10"
+              />
+          )}
+        </>
+      </ClickAwayListener>
   );
 };
