4c78ab62b7ed8486f08baf7d33599504a18fe56b
aphex <wenlogic@protonmail.com>
2022-09-21 16:29:17 +0100

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import {FC, useState} from "react";
-import {Footer, Navbar} from "components/organisms";
-import {PageContainer} from "components/atoms";
-import {RouteMap} from "./RouteMap";
-import {InformationArea} from "components/pages/InformationArea";
+import { FC, useState } from "react";
+import { Footer, Navbar } from "components/organisms";
+import { PageContainer } from "components/atoms";
+import { RouteMap } from "./RouteMap";
+import { InformationArea } from "components/pages/InformationArea";
+import { AppBackground } from "components/atoms/AppBackground";
 
 export const App: FC = () => {
   const [showInfoArea, setShowInfoArea] = useState(false);
 
   return (
     <div className="relative min-h-[100vh] overflow-x-hidden">
+      <AppBackground />
       <Navbar onHamburgerClick={() => setShowInfoArea(true)} />
       {showInfoArea && (
         <InformationArea
