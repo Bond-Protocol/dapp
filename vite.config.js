4335d1e4ba02fb50ea89807b482f6a0746301d12
spaceturtleship <89630561+spaceturtleship@users.noreply.github.com>
2022-10-05 11:38:18 +0900

    Upgrade rainbowkit, add node-globals-polyfill to fix vite missing Buffer etc (#94)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -10,7 +11,20 @@ export default defineConfig({
   build: {
     outDir: "../dist",
   },
-
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
   resolve: {
     alias: {
       components: path.join(__dirname, "./src/components"),
