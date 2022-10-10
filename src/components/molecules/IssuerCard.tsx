ee1af6f9a7de344421d66b781fe86c4ef2f75188
aphex <wenlogic@protonmail.com>
2022-10-10 12:03:34 +0000

    fix issuer name on issuer card

--- src/components/molecules/IssuerCard.tsx
+++ src/components/molecules/IssuerCard.tsx
@@ -1,21 +1,12 @@
 import { FC, useEffect, useState } from "react";
-import {Protocol, PROTOCOL_NAMES} from "@bond-protocol/bond-library";
+import { Protocol, PROTOCOL_NAMES } from "@bond-protocol/bond-library";
 import { useNavigate } from "react-router-dom";
 import { CalculatedMarket } from "@bond-protocol/contract-library";
-import {useBondPurchases} from "hooks/useBondPurchases";
+import { useBondPurchases } from "hooks/useBondPurchases";
 
 type IssuerCardProps = {
   issuer: Protocol;
   markets: CalculatedMarket[];
-};
-
-const splitCamelCaseText = (name: string) => {
-  return name
-    .split("")
-    .reduce((str, char) =>
-      char === char.toUpperCase() ? str + " " + char : str + char
-    )
-    .concat();
 };
 
 export const IssuerCard: FC<IssuerCardProps> = ({ issuer, markets }) => {
@@ -27,30 +18,18 @@ export const IssuerCard: FC<IssuerCardProps> = ({ issuer, markets }) => {
   useEffect(() => {
     setTbv(tbvByProtocol.get(issuer.name as PROTOCOL_NAMES) || 0);
   }, [tbvByProtocol]);
+  const handleClick = (name: string) => navigate("/issuers/" + name);
 
-  const logo = () => {
-    return issuer.logoUrl && issuer.logoUrl != ""
-      ? issuer.logoUrl
-      : "/placeholders/token-placeholder.png";
-  };
-
-  const handleClick = (event: any, name: string) => {
-    event.preventDefault();
-    navigate("/issuers/" + name);
-  };
+  const logo = issuer?.logoUrl || "/placeholders/token-placeholder.png";
 
   return (
     <div
-      className="border rounded-lg border-transparent bg-white/[.05] w-min w-[137px] max-w-[137px] p-5 flex flex-col items-center overflow-hidden hover:cursor-pointer hover:bg-white/10"
-      onClick={() => handleClick(event, issuer.id)}
+      className="border border-transparent rounded-lg bg-white/[.05] p-5 flex flex-col justify-between items-center hover:cursor-pointer hover:bg-white/10"
+      onClick={() => handleClick(issuer.id)}
     >
-      <img className="h-[64px] w-[64px]" src={logo()} />
-      <p className="my-2 font-bold tracking-wide text-center">
-        {issuer.name.length > 12
-          ? splitCamelCaseText(issuer.name)
-          : issuer.name}
-      </p>
-      <p className="text-xs text-light-primary-50">{markets.length} {markets.length === 1 ? "Market" : "Markets"}</p>
+      <img className="h-[64px] w-[64px]" src={logo} />
+      <p className="my-2 font-bold tracking-wide text-center">{issuer.name}</p>
+      <p className="text-xs text-light-primary-50">{markets.length} Markets</p>
       <p className="text-[10px] text-light-primary-300">
         TBV ${Math.floor(tbv)}
       </p>
