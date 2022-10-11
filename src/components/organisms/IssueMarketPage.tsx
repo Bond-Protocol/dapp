2365d87b7d79590a2dd4f4eabdb0b848f47fbb2b
aphex <wenlogic@protonmail.com>
2022-10-11 19:41:14 +0000

    remove log

--- src/components/organisms/IssueMarketPage.tsx
+++ src/components/organisms/IssueMarketPage.tsx
@@ -74,8 +74,6 @@ export const IssueMarketPage = (props: IssueMarketPageProps) => {
     props.onExecute(props.data);
   };
 
-  console.log({ data: props.data, networkchain: network.chain });
-
   //(TODO: (aphex) wagmi displays mainnet as homestead :(
   const selectedNetwork =
     network.chain?.network === "homestead"
