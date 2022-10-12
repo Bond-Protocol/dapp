c90a84da70352dbd7b17ffc12ff80b745454e5ed
aphex <52055541+wenfix@users.noreply.github.com>
2022-10-12 20:30:01 +0000

    fix/storybook-config (#123)

--- .storybook/main.js
+++ .storybook/main.js
@@ -22,10 +22,10 @@ module.exports = {
       path.resolve(__dirname, "../vite.config.ts")
     );
 
-    const { define, ...rest } = userConfig;
+    const { resolve, ...other } = userConfig;
 
     return mergeConfig(config, {
-      ...rest,
+      resolve,
       // manually specify plugins to avoid conflict
       plugins: [svgr()],
     });
