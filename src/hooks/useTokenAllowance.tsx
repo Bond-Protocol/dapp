da80ab22f3a5b8a6e1a9f17b33372aadd4975ad6
spaceturtleship <spaceturtleship@protonmail.com>
2022-10-27 14:42:32 +0900

    Remove unused import

--- src/hooks/useTokenAllowance.tsx
+++ src/hooks/useTokenAllowance.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { useAccount, useBalance } from "wagmi";
+import { useAccount } from "wagmi";
 import { providers } from "services/owned-providers";
 import { usePurchaseBond } from "./usePurchaseBond";
 import {
