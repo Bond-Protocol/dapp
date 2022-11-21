4876076210de253857776698a9fc3c3ed13363e2
spaceturtleship <spaceturtleship@protonmail.com>
2022-11-21 22:59:06 +0900

    Update mainnet contract addresses

--- packages/contract-library/src/modules/address-provider.ts
+++ packages/contract-library/src/modules/address-provider.ts
@@ -15,12 +15,12 @@ export type AddressesForType = {
 };
 
 const mainnetAddresses: ContractAddresses = {
-  authority: '0x007A0F3B057945db86408197DAa7C04373B5a94A',
-  aggregator: '0x007A66B9e719b3aBb2f3917Eb47D4231a17F5a0D',
-  fixedExpirationTeller: '0x007FE7c498A2Cf30971ad8f2cbC36bd14Ac51156',
-  fixedExpirationAuctioneer: '0x007FEA2a31644F20b0fE18f69643890b6F878AA6',
-  fixedTermTeller: '0x007F77B53ed0F058616335bc040cD326E125daE0',
-  fixedTermAuctioneer: '0x007F7A6012A5e03f6F388dd9F19Fd1D754Cfc128',
+  authority: '0x007A0F48A4e3d74Ab4234adf9eA9EB32f87b4b14',
+  aggregator: '0x007A66A2a13415DB3613C1a4dd1C942A285902d1',
+  fixedExpirationTeller: '0x007FE70dc9797C4198528aE43d8195ffF82Bdc95',
+  fixedExpirationAuctioneer: '0x007FEA32545a39Ff558a1367BBbC1A22bc7ABEfD',
+  fixedTermTeller: '0x007F7735baF391e207E3aA380bb53c4Bd9a5Fed6',
+  fixedTermAuctioneer: '0x007F7A1cb838A872515c8ebd16bE4b14Ef43a222',
 };
 
 const goerliAddresses: ContractAddresses = {
