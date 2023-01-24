b675241b16a11e613b431e421584fdb709616e13
aphex <wenlogic@protonmail.com>
2023-01-24 11:37:12 +0000

    smol lp improvements

--- apps/dapp/vite.config.js
+++ apps/dapp/vite.config.js
@@ -2,38 +2,13 @@ import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 
 export default defineConfig({
   plugins: [react(), svgr()],
   root: "src",
   envDir: "..",
-  define: {
-    global: "globalThis",
-  },
   build: {
     outDir: "../dist",
-    rollupOptions: {
-      plugins: [nodePolyfills()],
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
   },
   resolve: {
     alias: {
