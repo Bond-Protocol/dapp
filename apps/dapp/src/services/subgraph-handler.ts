9e5e52a9285bca724ff503557064d11f30870cab
aphex <wenlogic@protonmail.com>
2023-02-14 11:22:36 +0000

    more wip

--- apps/dapp/src/services/subgraph-handler.ts
+++ apps/dapp/src/services/subgraph-handler.ts
@@ -1,6 +1,5 @@
-import { useMemo } from "react";
 import { CHAIN_ID } from "@bond-protocol/bond-library";
-import { useQueries, UseQueryResult } from "react-query";
+import { UseQueryResult } from "react-query";
 import { environment } from "src/environment";
 import { mainnetEndpoints, testnetEndpoints } from "./subgraph-endpoints";
 
@@ -45,52 +44,3 @@ export const getSubgraphQueriesPerChainFn = (
   });
   return queries;
 };
-
-// export const useSubgraphQueries = (
-//   query: ({}: any, {}: any, {}: any) => UseQueryResult<any, any>,
-//   variables?: {}
-// ) => {
-//   const queries = endpoints.map((endpoint) => ({
-//     queryKey: endpoint.url + "--" + query.name.toString(),
-//     queryFn: () =>
-//       query(
-//         { endpoint: endpoint.url },
-//         { queryKey: endpoint.url + "--" + query.name.toString(), ...variables },
-//         { enabled: testnet ? !!testnet : !testnet }
-//       ),
-//   }));
-
-//   const results = useQueries(queries);
-
-//   return {
-//     results,
-//   };
-// };
-
-// export const useSubgraphForChain = (
-//   query: ({}: any, {}: any, {}: any) => UseQueryResult<any, any>,
-//   func: (chain: CHAIN_ID) => any,
-//   fieldName: string
-// ) => {
-//   const queries = endpoints.map((endpoint) => {
-//     const variables = {
-//       queryKey: endpoint.url + "--" + query.name.toString(),
-//     };
-//     // @ts-ignore
-//     variables[fieldName] = func(endpoint.chain);
-
-//     return {
-//       queryKey: endpoint.url + "--" + query.name.toString(),
-//       queryFn: () =>
-//         query({ endpoint: endpoint.url }, variables, {
-//           enabled: testnet ? !!testnet : !testnet,
-//         }),
-//     };
-//   });
-
-//   console.log({ queries });
-//   const results = useQueries(queries);
-
-//   console.log({ results });
-//   return results;
-// };
