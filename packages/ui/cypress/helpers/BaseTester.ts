67a86634fefbfad317c07b47ef1d9f1ef93fde0d
aphex <wenlogic@protonmail.com>
2023-04-14 10:42:02 +0100

    wip

--- packages/ui/cypress/helpers/BaseTester.ts
+++ packages/ui/cypress/helpers/BaseTester.ts
@@ -1,15 +1,5 @@
 export default class BaseTester {
-  protected get(value: string) {
+  get(value: string) {
     return cy.get(value);
-  }
-
-  protected selectDropdownOption(field: string, option: string) {
-    const select = cy.get(field);
-
-    select.click();
-
-    select.next().children("ul").contains(option).first().click();
-
-    return select;
   }
 }
