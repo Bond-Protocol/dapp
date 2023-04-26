972b238c86e9df4074f70cc2c1132115980504e6
aphex <wenlogic@protonmail.com>
2023-04-26 16:24:06 +0100

    allowance valid on tenderly

--- packages/contract-library/src/core/encode.ts
+++ packages/contract-library/src/core/encode.ts
@@ -1,10 +1,17 @@
 import { defaultAbiCoder } from '@ethersproject/abi';
+import { ethers } from 'ethers';
 
+export const ERC20_APPROVE_SIGNATURE = 'approve(address,uint256)';
 export const ERC20_APPROVE_SIGNATURE_TYPES = ['address', 'uint256'];
 
 export const getAllowanceTxBytes = (spender: string, amount: string) => {
-  return defaultAbiCoder.encode(ERC20_APPROVE_SIGNATURE_TYPES, [
+  const encodedData = defaultAbiCoder.encode(ERC20_APPROVE_SIGNATURE_TYPES, [
     spender,
     amount,
+  ]);
+
+  return ethers.utils.hexConcat([
+    ethers.utils.id(ERC20_APPROVE_SIGNATURE),
+    encodedData,
   ]);
 };
