77ed48adad41a5963d34511e7e0fe5ec087bf52d
spaceturtleship <89630561+spaceturtleship@users.noreply.github.com>
2023-05-26 09:33:34 +0100

    Update dex finance logo (#412)
    * Add dexfinance
    
    * Update dex finance logo
    
    * Update dex finance logo

--- packages/data-library/src/protocols/dexfinance.ts
+++ packages/data-library/src/protocols/dexfinance.ts
@@ -4,8 +4,10 @@ import { ProtocolDefinition } from "../public-types";
 /* Protocol Template */
 export default {
   name: "Dex Finance", // Your protocol's display name, formatted as it will appear in the BondProtocol dApp
-  description: "Dex Finance provides an ecosystem of financial products that aims to help users navigate through the intricacies DeFi via sustainable real yields.", // A short protocol description
-  logoUrl: "https://dexfinance-fullapp-develop-3breg.ondigitalocean.app/static/media/logo.ed7f76eb9ae23fe7a1141d23e97aba44.svg",
+  description:
+    "Dex Finance provides an ecosystem of financial products that aims to help users navigate through the intricacies DeFi via sustainable real yields.", // A short protocol description
+  logoUrl:
+    "https://www.dexfinance.com/wp-content/uploads/2022/10/Dex-Finance-Icon.svg",
   // Links to landing page, socials and docs
   links: {
     // NOTE: bonding is inscribed on the whitepaper prior to TGE.
@@ -35,7 +37,8 @@ export default {
     {
       name: "DexFi Governance",
       symbol: "gDEX",
-      logoUrl: "https://dexfinance-fullapp-develop-3breg.ondigitalocean.app/static/media/gDEX.77d2729bc4db1ec442aabeebe31e8bd1.svg",
+      logoUrl:
+        "https://dexfinance-fullapp-develop-3breg.ondigitalocean.app/static/media/gDEX.77d2729bc4db1ec442aabeebe31e8bd1.svg",
       priceSources: [
         {
           source: "custom",
@@ -43,13 +46,19 @@ export default {
           providerChainId: CHAIN_ID.ARBITRUM_MAINNET,
         },
       ],
-      purchaseLinks: { [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.dexfinance.com/swap" }, // Where to acquire your token
-      addresses: { [CHAIN_ID.ARBITRUM_MAINNET]: "0xda064d44871bA31Dae33A65B3E23B37E44E9168A" },
+      purchaseLinks: {
+        [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.dexfinance.com/swap",
+      }, // Where to acquire your token
+      addresses: {
+        [CHAIN_ID.ARBITRUM_MAINNET]:
+          "0xda064d44871bA31Dae33A65B3E23B37E44E9168A",
+      },
     },
     {
       name: "USDEX+",
       symbol: "USDEX+",
-      logoUrl: "https://dexfinance-fullapp-develop-3breg.ondigitalocean.app/static/media/USDEX+.65ec4876ac2d3bea6b747406142e90a9.svg",
+      logoUrl:
+        "https://dexfinance-fullapp-develop-3breg.ondigitalocean.app/static/media/USDEX+.65ec4876ac2d3bea6b747406142e90a9.svg",
       priceSources: [
         {
           source: "custom",
@@ -57,8 +66,13 @@ export default {
           providerChainId: CHAIN_ID.ARBITRUM_MAINNET,
         },
       ],
-      purchaseLinks: { [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.dexfinance.com/swap" }, // Where to acquire your token
-      addresses: { [CHAIN_ID.ARBITRUM_MAINNET]: "0x2c093F7d7Db41057A2ddD3cB1AD8b4E9C3dFfBec" },
+      purchaseLinks: {
+        [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.dexfinance.com/swap",
+      }, // Where to acquire your token
+      addresses: {
+        [CHAIN_ID.ARBITRUM_MAINNET]:
+          "0x2c093F7d7Db41057A2ddD3cB1AD8b4E9C3dFfBec",
+      },
     },
     {
       name: "USDEX+/gDEX LP",
@@ -66,14 +80,16 @@ export default {
       // @ts-ignore
       lpType: SUPPORTED_LP_TYPES.UNISWAP_V2,
       addresses: {
-        [CHAIN_ID.ARBITRUM_MAINNET]: "0x62118a631D030C3023dD3001b7ce96aa042ffb1E",
+        [CHAIN_ID.ARBITRUM_MAINNET]:
+          "0x62118a631D030C3023dD3001b7ce96aa042ffb1E",
       },
       token0Address: "0x2c093F7d7Db41057A2ddD3cB1AD8b4E9C3dFfBec",
       token1Address: "0xda064d44871bA31Dae33A65B3E23B37E44E9168A",
       purchaseLinks: {
-        [CHAIN_ID.ARBITRUM_MAINNET]: "https://app.dexfinance.com/add/0xda064d44871bA31Dae33A65B3E23B37E44E9168A/0x2c093F7d7Db41057A2ddD3cB1AD8b4E9C3dFfBec",
+        [CHAIN_ID.ARBITRUM_MAINNET]:
+          "https://app.dexfinance.com/add/0xda064d44871bA31Dae33A65B3E23B37E44E9168A/0x2c093F7d7Db41057A2ddD3cB1AD8b4E9C3dFfBec",
       },
       priceSources: [],
-    }
+    },
   ],
 } as ProtocolDefinition;
