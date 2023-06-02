5b0576e939de12846fd14f175ae3731d58925c1c
spaceturtleship <spaceturtleship@protonmail.com>
2023-06-02 09:26:19 +0100

    Remove obsolete queries and hook

--- apps/dapp/src/hooks/index.ts
+++ apps/dapp/src/hooks/index.ts
@@ -9,7 +9,6 @@ export * from "./useFetchMarkets";
 export * from "./useGasPrice";
 export * from "./useMarkets";
 export * from "./useMyBonds";
-export * from "./useLoadMarkets";
 export * from "./usePurchaseBond";
 export * from "./useTokenAllowance";
 export * from "./useTokenPricesHistory";
