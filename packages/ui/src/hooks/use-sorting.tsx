1754c0e182b39944ee01d442100162aaba3b7b65
aphex <wenlogic@protonmail.com>
2023-06-15 15:51:50 +0100

    fix table sorting

--- packages/ui/src/hooks/use-sorting.tsx
+++ packages/ui/src/hooks/use-sorting.tsx
@@ -16,30 +16,12 @@ export const useSorting = (
         [...data].sort((a, b) => {
           let current = a[sortField]?.sortValue || a[sortField]?.value;
           let next = b[sortField]?.sortValue || b[sortField]?.value;
-          if (!current) return 1;
-          if (!next) return -1;
-          if (!current && !next) return 0;
 
-          if (isNaN(current) || current === Infinity || current === -Infinity)
-            current = 0;
-          if (isNaN(next) || next === Infinity || next === -Infinity) next = 0;
-
-          const numA = parseFloat(current);
-          const numB = parseFloat(next);
-
-          if (numA < numB) {
-            return sortOrder === "asc" ? -1 : 1;
-          }
-          if (numA > numB) {
-            return sortOrder === "asc" ? 1 : -1;
-          }
-          return 0;
-
-          // return (
-          //   current.toString().localeCompare(next.toString(), "en", {
-          //     numeric: true,
-          //   }) * (sortOrder === "asc" ? 1 : -1)
-          // );
+          return (
+            current.toString().localeCompare(next.toString(), "en", {
+              numeric: true,
+            }) * (sortOrder === "asc" ? 1 : -1)
+          );
         });
       setData(sorted);
     }
@@ -57,23 +39,6 @@ export const handleSorting = (
   return data.sort((a, b) => {
     const current = a[sortField]?.sortValue || a[sortField].value;
     const next = b[sortField]?.sortValue || b[sortField].value;
-
-    if (!current) return 1;
-    if (!next) return -1;
-    if (!current && !next) return 0;
-
-    if (!isNaN(current) && !isNaN(next)) {
-      const numA = parseFloat(current);
-      const numB = parseFloat(next);
-
-      if (numA < numB) {
-        return sortOrder === "asc" ? -1 : 1;
-      }
-      if (numA > numB) {
-        return sortOrder === "asc" ? 1 : -1;
-      }
-      return 0;
-    }
 
     return (
       current.toString().localeCompare(next.toString(), "en", {
