3f0bd9e1ed26263370cbb55f40b807fabaf9258d
aphex <wenlogic@protonmail.com>
2023-08-11 17:28:18 +0100

    remove unused metadata file

--- apps/lp/src/data/metadata-config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  title: "Bond Protocol",
-  description:
-    "Acquire assets, own liquidity and diversify your treasury on the permissionless bond marketplace",
-  url: "bondprotocol.finance",
-  twitterHandle: "@bond_protocol",
-  image: "https://bondprotocol.finance/logo-long.svg",
-};
