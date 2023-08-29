be57b7ab95b24b264af18ba0b5d9a52c42a12baf
spaceturtleship <spaceturtleship@protonmail.com>
2023-08-29 12:25:56 +0100

    Add orders/new request to test

--- apps/dapp/src/components/common/SignInButton.tsx
+++ apps/dapp/src/components/common/SignInButton.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { useAccount, useNetwork, useSignMessage } from 'wagmi'
+import {useAccount, useNetwork, useSignMessage, useSignTypedData} from 'wagmi'
 import { SiweMessage } from 'siwe'
 import axios from "axios";
+import {BigNumber} from "ethers";
 
 function SignInButton({
                           onSuccess,
@@ -75,9 +76,10 @@ function SignInButton({
 }
 
 export function Profile() {
-    const { isConnected } = useAccount()
-    const { chain } = useNetwork()
-    const { address } = useAccount()
+    const { isConnected } = useAccount();
+    const { chain } = useNetwork();
+    const { address } = useAccount();
+    const { signTypedDataAsync } = useSignTypedData();
 
     const [state, setState] = React.useState<{
         address?: string
@@ -90,7 +92,8 @@ export function Profile() {
         if (!chainId || !address) return
 
         try {
-            const testRes = await axios.get(`http://localhost:8080/auth/test/` + address, {
+            const testRes = await axios.get(
+                `http://localhost:8080/auth/test/` + address, {
                 headers: {
                     'Authorization': `Bearer ${sessionStorage.getItem("access_token")}`,
                     'x-chain-id': chainId.toString(),
@@ -103,7 +106,8 @@ export function Profile() {
         }
 
         try {
-            const testRes2 = await axios.get(`http://localhost:8080/auth/test/0x0000000000000000000000000000000000000000`, {
+            const testRes2 = await axios.get(
+                `http://localhost:8080/auth/test/0x0000000000000000000000000000000000000000`, {
                 headers: {
                     'Authorization': `Bearer ${sessionStorage.getItem("access_token")}`,
                     'x-chain-id': chainId.toString(),
@@ -111,6 +115,84 @@ export function Profile() {
                     'x-settlement': "",
                 },
             });
+        } catch (e) {
+            console.log(e);
+        }
+
+        console.log({
+            market_id: BigNumber.from(1),
+            recipient: address as `0x${string}`,
+            referrer: address as `0x${string}`,
+            amount: BigNumber.from(100),
+            min_amount_out: BigNumber.from(98),
+            max_fee: BigNumber.from(1),
+            submitted: BigNumber.from(Date.now()),
+            deadline: BigNumber.from(Date.now() + 86400),
+            user: address as `0x${string}`,
+            signature: "",
+        })
+
+        const types = {
+            Order: [
+                { name: 'market_id', type: 'uint256' },
+                { name: 'recipient', type: 'address' },
+                { name: 'referrer', type: 'address' },
+                { name: 'amount', type: 'uint256' },
+                { name: 'min_amount_out', type: 'uint256' },
+                { name: 'max_fee', type: 'uint256' },
+                { name: 'submitted', type: 'uint256' },
+                { name: 'deadline', type: 'uint256' },
+                { name: 'user', type: 'address' }
+            ]
+        };
+
+        const message = {
+            order: {
+                market_id: "1",
+                recipient: address as `0x${string}`,
+                referrer: address as `0x${string}`,
+                amount: "100",
+                min_amount_out: "98",
+                max_fee: "1",
+                submitted: Date.now().toString(),
+                deadline: (Date.now() + 86400).toString(),
+                user: address as `0x${string}`,
+            }
+        } as const;
+
+        const signature = await signTypedDataAsync({
+            domain: {},
+            value: message.order,
+            types: types,
+        });
+
+        const order = {
+            market_id: "1",
+            recipient: address as `0x${string}`,
+            referrer: address as `0x${string}`,
+            amount: "100",
+            min_amount_out: "98",
+            max_fee: "1",
+            submitted: Date.now().toString(),
+            deadline: (Date.now() + 86400).toString(),
+            user: address as `0x${string}`,
+            signature: signature,
+        }
+
+        try {
+            const orderTest = await axios.post(
+                `http://localhost:8080/orders/new`,
+                {
+                    ...order
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-chain-id': chainId.toString(),
+                        'x-aggregator': "0x0000000000000000000000000000000000000001",
+                        'x-settlement': "0x0000000000000000000000000000000000000001",
+                    },
+                })
         } catch (e) {
             console.log(e);
         }
