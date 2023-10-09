106f0650d1d54ec5fc91718388c78efaea79a893
aphex <wenlogic@protonmail.com>
2023-10-09 18:04:41 +0100

    wip

--- apps/dapp/src/components/lists/PastMarketList.tsx
+++ apps/dapp/src/components/lists/PastMarketList.tsx
@@ -1,5 +1,5 @@
 import { useMarkets } from "context/market-context";
-import { PaginatedTable } from "ui";
+import { Filter, PaginatedTable } from "ui";
 import { pastMarketColumns } from "./UserMarketList";
 
 const tableColumns = pastMarketColumns.map((t) => ({
@@ -7,11 +7,24 @@ const tableColumns = pastMarketColumns.map((t) => ({
   width: "w-full",
 }));
 
+const filters: Filter[] = [
+  {
+    type: "switch",
+    id: "knownValue",
+    label: "Show unknown value",
+    handler: (args) => {
+      console.log({ args });
+
+      return true;
+    },
+  },
+];
 export const PastMarketList = () => {
   const { closedMarkets } = useMarkets();
 
   return (
     <PaginatedTable
+      filters={filters}
       title="Past Markets"
       defaultSort="conclusion"
       columns={tableColumns}
