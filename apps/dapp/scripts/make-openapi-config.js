0564dd9d444a420424466629db487234035ad211
aphex <wenlogic@protonmail.com>
2023-10-27 09:31:02 +0100

    remove unused script

--- apps/dapp/scripts/make-openapi-config.js
+++ apps/dapp/scripts/make-openapi-config.js
@@ -1,13 +1,11 @@
 const yaml = require("js-yaml");
 const fs = require("fs");
 const path = require("path");
-const expandParameters = require("./parse-openapi-parameters");
 
 const data = fs.readFileSync(path.join(__dirname, "..", "openapi.yml"), "utf8");
 
 //Convert Yml object to JSON
 const yamlData = yaml.load(data);
-//expandParameters(yamlData);
 
 //Write JSON to Yml
 const jsonData = JSON.stringify(yamlData);
