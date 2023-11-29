b7637e7eb3db98df92cca9e0f1435b0853095624
brightiron <brightiron@protonmail.com>
2023-11-29 17:46:28 -0600

    resolve imports

--- apps/dapp/src/hooks/useTokenPricesHistory.tsx
+++ apps/dapp/src/hooks/useTokenPricesHistory.tsx
@@ -1,5 +1,5 @@
-import { useQuery } from "react-query";
 import type { Token } from "types";
+import { useQuery } from "@tanstack/react-query";
 import { getTokenPriceHistory } from "services";
 
 export const useTokenPriceHistory = (token: Token, dayRange = 90) => {
--- apps/dapp/src/services/subgraph-endpoints.tsx
+++ apps/dapp/src/services/subgraph-endpoints.tsx
@@ -1,4 +1,4 @@
-import { UseQueryResult } from "react-query";
+import { UseQueryResult } from "@tanstack/react-query";
 import { environment } from "src/environment";
 import { mainnetSubgraphs, testnetSubgraphs } from "src/config";
 import { CHAIN_ID } from "types";
