7fbffde809c33ce5a40b4b42cc81806e87ec193a
brightiron <brightiron@protonmail.com>
2023-11-29 20:01:00 -0600

    wip subgraph provider refactor

---
+++ apps/dapp/src/hooks/useGetGlobalData.tsx
@@ -0,0 +1,81 @@
+import { useQuery, useQueries, UseQueryResult } from "@tanstack/react-query";
+import {
+  testnetEndpoints,
+  mainnetEndpoints,
+} from "services/subgraph-endpoints";
+import { environment } from "src/environment";
+import {
+  GetGlobalDataDocument,
+  GetGlobalDataQuery,
+} from "src/generated/graphql";
+import { DocumentNode } from "graphql";
+import { concatSubgraphQueryResultArrays } from "src/utils/concatSubgraphQueryResultArrays";
+import { CHAIN_ID } from "@bond-protocol/contract-library";
+import { queryAllEndpoints } from "src/utils/queryAllEndpoints";
+
+export type TypedDocumentNode<
+  Result = Record<string, unknown>,
+  Variables = Record<string, unknown>
+> = DocumentNode;
+
+/**
+ * Fetches global data from all subgraphs
+ */
+export const useGetGlobalData = () => {
+  const isTestnet = environment.isTestnet;
+  const endpoints = isTestnet ? testnetEndpoints : mainnetEndpoints;
+
+  return useQueries({
+    queries: queryAllEndpoints<GetGlobalDataQuery>({
+      document: GetGlobalDataDocument,
+    }),
+    combine: (responses) => {
+      const filteredResponses = responses.filter(
+        (response): response is UseQueryResult<GetGlobalDataQuery> =>
+          response?.data !== undefined
+      );
+
+      console.log(responses, "response");
+      if (filteredResponses.length === 0)
+        return {
+          data: {
+            markets: [],
+            totalPurchases: 0,
+            uniqueBonders: 0,
+            subgraphTokens: [],
+          },
+          isLoading: true,
+        };
+
+      const totalPurchases = concatSubgraphQueryResultArrays(
+        filteredResponses,
+        "purchaseCounts"
+      ).reduce((total, pc) => total + Number(pc.count), 0);
+      const uniqueBonders = concatSubgraphQueryResultArrays(
+        filteredResponses,
+        "uniqueTokenBonderCounts"
+      ).reduce((total, ub) => total + Number(ub.count), 0);
+
+      const subgraphTokens = concatSubgraphQueryResultArrays(
+        filteredResponses,
+        "tokens"
+      );
+      const markets = subgraphTokens
+        .map((token) => token.markets)
+        .flat()
+        .filter(
+          (market): market is NonNullable<typeof market> =>
+            market !== null && market !== undefined
+        );
+      return {
+        data: {
+          markets,
+          totalPurchases,
+          uniqueBonders,
+          subgraphTokens,
+        },
+        isLoading: filteredResponses.some((response) => response.isLoading),
+      };
+    },
+  });
+};
