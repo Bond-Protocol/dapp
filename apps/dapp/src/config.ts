67e1233f6546b9451cdb7ae0d8d8e6d08d19edd2
aphex <wenlogic@protonmail.com>
2024-06-06 00:11:35 +0100

    move subgraphs out of env into config file

--- apps/dapp/src/config.ts
+++ apps/dapp/src/config.ts
@@ -22,3 +22,17 @@ export const orderApiServerMap = {
     description: "Production Server",
   },
 };
+
+export const mainnetSubgraphs = {
+  "1": "https://api.thegraph.com/subgraphs/name/bond-protocol/bond-protocol-mainnet", //mainnet
+  "42161":
+    "https://subgraph.satsuma-prod.com/44c4cc082f74/spaces-team/bond-protocol-arbitrum/api", //arb
+  "10": "https://api.thegraph.com/subgraphs/name/bond-protocol/bond-protocol-optimism-mainnet", //op
+  "8453":
+    "https://subgraph.satsuma-prod.com/8cad5c83fb09/spaces-team/bond-protocol-base/api", //base
+  "137":
+    "https://api.thegraph.com/subgraphs/name/bond-protocol/bond-protocol-polygon-mainnet", //polygon
+  "56": "https://api.goldsky.com/api/public/project_clu16lu24lqh201x9f0qh135t/subgraphs/bond-protocol-bsc/0.0.1/gn", //bsc
+  "34443":
+    "https://api.goldsky.com/api/public/project_clu16lu24lqh201x9f0qh135t/subgraphs/bond-protocol-mode/0.0.1/gn", //mode
+};
--- apps/dapp/src/services/subgraph-endpoints.tsx
+++ apps/dapp/src/services/subgraph-endpoints.tsx
@@ -1,33 +1,13 @@
 import { UseQueryResult } from "react-query";
 import { environment } from "src/environment";
+import { mainnetSubgraphs } from "src/config";
 import { CHAIN_ID } from "types";
 
 /**List of available subgraph endpoint urls indexed by chain*/
 export const subgraphEndpoints = {
-  [CHAIN_ID.ETHEREUM_MAINNET]: `${
-    import.meta.env.VITE_ETHEREUM_MAINNET_SUBGRAPH_ENDPOINT
-  }`,
+  ...mainnetSubgraphs,
   [CHAIN_ID.GOERLI_TESTNET]: `${
     import.meta.env.VITE_ETHEREUM_TESTNET_SUBGRAPH_ENDPOINT
-  }`,
-  [CHAIN_ID.ARBITRUM_MAINNET]: `${
-    import.meta.env.VITE_ARBITRUM_MAINNET_SUBGRAPH_ENDPOINT
-  }`,
-  [CHAIN_ID.OPTIMISM_MAINNET]: `${
-    import.meta.env.VITE_OPTIMISM_MAINNET_SUBGRAPH_ENDPOINT
-  }`,
-  [CHAIN_ID.POLYGON_MAINNET]: `${
-    import.meta.env.VITE_POLYGON_MAINNET_SUBGRAPH_ENDPOINT
-  }`,
-  [CHAIN_ID.BASE_MAINNET]: `${
-    import.meta.env.VITE_BASE_MAINNET_SUBGRAPH_ENDPOINT
-  }`,
-
-  [CHAIN_ID.BSC_MAINNET]: `${
-    import.meta.env.VITE_BSC_MAINNET_SUBGRAPH_ENDPOINT
-  }`,
-  [CHAIN_ID.MODE_MAINNET]: `${
-    import.meta.env.VITE_BSC_MAINNET_SUBGRAPH_ENDPOINT
   }`,
 
   [CHAIN_ID.ARBITRUM_GOERLI_TESTNET]: `${
