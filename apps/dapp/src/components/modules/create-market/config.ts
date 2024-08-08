954ffa4605ec35589d4984aefb5362d8a8100c5d
aphex <wenlogic@protonmail.com>
2024-08-08 13:04:56 +0100

    disabled fixed expiry options in create market when auctioneers are not deployed

--- apps/dapp/src/components/modules/create-market/config.ts
+++ apps/dapp/src/components/modules/create-market/config.ts
@@ -1,2 +1,5 @@
 /** Chains where the Oracle Auctioneers are not avaiable*/
 export const unavailableOracleChains = [1, 8453, 84532, 56, 34443];
+
+/** Chains where the Fixed Expiry Auctioneers are not avaiable*/
+export const unavailableFixedExpiry = [34443, 80084];
