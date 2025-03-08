dfd2900cf6a78095115c5653126199f6f66ba477
aphex <wenlogic@protonmail.com>
2025-03-08 16:34:44 +0000

    disable polyfills for now

--- apps/dapp/vite.config.js
+++ apps/dapp/vite.config.js
@@ -26,21 +26,6 @@ export default defineConfig({
   envDir: "..",
   define: {
     __COMMIT_HASH__: JSON.stringify(commitHash),
-    global: (() => {
-      let globalVariable = "globalThis";
-      try {
-        // Try to import @safe-global/safe-apps-provider
-        require.resolve("@safe-global/safe-apps-provider");
-        // Try to import @safe-global/safe-apps-sdk
-        require.resolve("@safe-global/safe-apps-sdk");
-        // If both modules are found, return the custom global variable
-        globalVariable = "global";
-      } catch (e) {
-        // If either module is not found, fallback to globalThis
-        globalVariable = "globalThis";
-      }
-      return globalVariable;
-    })(),
   },
   build: {
     outDir: "../dist",
@@ -54,24 +39,6 @@ export default defineConfig({
   },
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: (() => {
-          let globalVariable = "globalThis";
-          try {
-            // Try to import @safe-global/safe-apps-provider
-            require.resolve("@safe-global/safe-apps-provider");
-            // Try to import @safe-global/safe-apps-sdk
-            require.resolve("@safe-global/safe-apps-sdk");
-            // If both modules are found, return the custom global variable
-            globalVariable = "global";
-          } catch (e) {
-            // If either module is not found, fallback to globalThis
-            globalVariable = "globalThis";
-          }
-          return globalVariable;
-        })(),
-      },
       target: "es2020",
       // Enable esbuild polyfill plugins
       plugins: [
